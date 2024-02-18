7fe9c3f251cc3bc199861dc3aeae2d602c19b1b0
devmarciosieto <mmmsieto@hotmail.com>
2024-02-18 14:21:19 -0300

    add @TestMethodOrder(MethodOrderer.OrderAnnotation.class)

--- src/test/java/br/com/mmmsieto/infra/UserServiceWithUserMemoryRepositoryTest.java
+++ src/test/java/br/com/mmmsieto/infra/UserServiceWithUserMemoryRepositoryTest.java
@@ -5,17 +5,18 @@ import br.com.mmmsieto.financial.domain.entity.User;
 import br.com.mmmsieto.financial.domain.exceptions.ValidationException;
 import br.com.mmmsieto.financial.infra.UserMemoryRepository;
 import br.com.mmmsieto.financial.service.UserService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static br.com.mmmsieto.builders.UserBuilderTest.builder;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserServiceWithUserMemoryRepositoryTest {
 
     private static UserService service = new UserService(new UserMemoryRepository());
 
     @Test
+    @Order(1)
     @DisplayName("Should save a valid user")
     void should_save_a_valid_user() {
         User user = service.save(builder().id(null).build());
@@ -23,6 +24,7 @@ public class UserServiceWithUserMemoryRepositoryTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Should reject an existing user")
     void should_reject_an_existing_user() {
 
