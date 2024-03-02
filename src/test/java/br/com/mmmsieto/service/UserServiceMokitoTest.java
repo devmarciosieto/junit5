490fd96f99a07ccead7231b4d733abb1e869fccb
devmarciosieto <mmmsieto@hotmail.com>
2024-03-02 15:47:00 -0300

    add tests

--- src/test/java/br/com/mmmsieto/service/UserServiceMokitoTest.java
+++ src/test/java/br/com/mmmsieto/service/UserServiceMokitoTest.java
@@ -1,7 +1,6 @@
 package br.com.mmmsieto.service;
 
 import br.com.mmmsieto.builders.UserBuilderTest;
-import br.com.mmmsieto.financial.domain.builders.UserBuilder;
 import br.com.mmmsieto.financial.domain.entity.User;
 import br.com.mmmsieto.financial.repository.UserRepository;
 import br.com.mmmsieto.financial.service.UserService;
@@ -11,7 +10,8 @@ import org.mockito.Mockito;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 
@@ -66,6 +66,52 @@ public class UserServiceMokitoTest {
         verify(repository, atLeastOnce()).getUserByEmail(EMAIL);
         verify(repository, never()).getUserByEmail("other@gmail.com");
         verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    @DisplayName("Should return a user by email by calling the method more than once")
+    void should_return_a_user_by_email_by_calling_the_method_more_than_once() {
+
+        final String EMAIL = "email@hotmail.com";
+
+        UserRepository repository = mock(UserRepository.class);
+        userService = new UserService(repository);
+
+//        when(repository.getUserByEmail(EMAIL))
+//                .thenReturn(Optional.of(UserBuilderTest.builder().email("email01@gmail.com").build()))
+//                .thenReturn(Optional.of(UserBuilderTest.builder().email("email02@gmail.com").build()))
+//                .thenReturn(Optional.of(UserBuilderTest.builder().email("email03@gmail.com").build()))
+//                .thenReturn(Optional.of(UserBuilderTest.builder().email("email04@gmail.com").build()));
+
+        when(repository.getUserByEmail(EMAIL))
+                .thenReturn(Optional.of(UserBuilderTest.builder().email("email01@gmail.com").build()),
+                        Optional.of(UserBuilderTest.builder().email("email02@gmail.com").build()),
+                        Optional.of(UserBuilderTest.builder().email("email03@gmail.com").build()),
+                        Optional.of(UserBuilderTest.builder().email("email04@gmail.com").build()));
+
+
+        Optional<User> user = userService.getUserByEmail(EMAIL);
+        System.out.println(user);
+
+        user = userService.getUserByEmail(EMAIL);
+        System.out.println(user);
+
+        user = userService.getUserByEmail(EMAIL);
+        System.out.println(user);
+
+        user = userService.getUserByEmail("other@gmail.com");
+        System.out.println(user);
+
+        user = userService.getUserByEmail(EMAIL);
+        System.out.println(user);
+
+        user = userService.getUserByEmail(EMAIL);
+        System.out.println(user);
+
+
+
+        verify(repository, atLeastOnce()).getUserByEmail(EMAIL);
+        verify(repository, never()).getUserByEmail("other1@gmail.com");
     }
 
 }
