c71c3ac72ef23b04b64a4c92de7d802b550e8575
devmarciosieto <mmmsieto@hotmail.com>
2024-03-01 23:05:56 -0300

    add mockito method

--- src/test/java/br/com/mmmsieto/service/UserServiceMokitoTest.java
+++ src/test/java/br/com/mmmsieto/service/UserServiceMokitoTest.java
@@ -1,5 +1,7 @@
 package br.com.mmmsieto.service;
 
+import br.com.mmmsieto.builders.UserBuilderTest;
+import br.com.mmmsieto.financial.domain.builders.UserBuilder;
 import br.com.mmmsieto.financial.domain.entity.User;
 import br.com.mmmsieto.financial.repository.UserRepository;
 import br.com.mmmsieto.financial.service.UserService;
@@ -9,7 +11,8 @@ import org.mockito.Mockito;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 public class UserServiceMokitoTest {
@@ -27,15 +30,42 @@ public class UserServiceMokitoTest {
     }
 
     @Test
-    @DisplayName("Should return a user by email")
-    void should_return_a_user_by_email() {
+    @DisplayName("should return a non-existent empty user")
+    void should_return_a_non_existent_empty_user() {
 
         UserRepository repository = Mockito.mock(UserRepository.class);
         userService = new UserService(repository);
 
+        // como esse já é o comportamento patrão, não precisa
+//        Mockito.when(repository.getUserByEmail("email@hotmail.com"))
+//                .thenReturn(Optional.empty());
+
         Optional<User> user = userService.getUserByEmail("email@hotmail.com");
         assertTrue(user.isEmpty());
 
+    }
+
+    @Test
+    @DisplayName("Should return a user by email")
+    void should_return_a_user_by_email() {
+
+        final String EMAIL = "email@hotmail.com";
+
+        UserRepository repository = mock(UserRepository.class);
+        userService = new UserService(repository);
+
+        when(repository.getUserByEmail(EMAIL))
+                .thenReturn(Optional.of(UserBuilderTest.builder().email(EMAIL).build()));
+
+        Optional<User> user = userService.getUserByEmail(EMAIL);
+        assertTrue(user.isPresent());
+        assertEquals(EMAIL, user.get().getEmail());
+
+        verify(repository).getUserByEmail(EMAIL);
+        verify(repository, times(1)).getUserByEmail(EMAIL);
+        verify(repository, atLeastOnce()).getUserByEmail(EMAIL);
+        verify(repository, never()).getUserByEmail("other@gmail.com");
+        verifyNoMoreInteractions(repository);
     }
 
 }
