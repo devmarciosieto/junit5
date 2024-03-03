8047dd261f8e5c20429189cdce3a3c5e9fcf408f
devmarciosieto <mmmsieto@hotmail.com>
2024-03-02 22:28:52 -0300

    add tests

--- src/test/java/br/com/mmmsieto/service/UserServiceMokitoTest.java
+++ src/test/java/br/com/mmmsieto/service/UserServiceMokitoTest.java
@@ -2,45 +2,90 @@ package br.com.mmmsieto.service;
 
 import br.com.mmmsieto.builders.UserBuilderTest;
 import br.com.mmmsieto.financial.domain.entity.User;
+import br.com.mmmsieto.financial.domain.exceptions.ValidationException;
 import br.com.mmmsieto.financial.repository.UserRepository;
 import br.com.mmmsieto.financial.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceMokitoTest {
 
+    @InjectMocks
     private UserService userService;
+
+    @Mock
+    private UserRepository repository;
+
+    final String EMAIL = "email@hotmail.com";
+    final String EMAIL_OTHER = "other@gmail.com";
+
+
+//    void setup() {
+    // no longer needed because of @ExtendWith(MockitoExtension.class)
+//    @BeforeEach
+//        MockitoAnnotations.openMocks(this);
+//    }
+
+//    @AfterEach
+//    void tearDown(){
+//        verifyNoMoreInteractions(repository);
+//    }
 
     @Test
     @DisplayName("Should save a user successfully")
     void should_save_a_user_successfully() {
 
-        UserRepository repository = Mockito.mock(UserRepository.class);
-        userService = new UserService(repository);
+        User userToSave = UserBuilderTest.builder().id(null).build();
 
+        when(repository.getUserByEmail(userToSave.getEmail()))
+                .thenReturn(Optional.empty());
 
+        when(repository.save(userToSave)).thenReturn(UserBuilderTest.builder().build());
+
+        User user = userService.save(userToSave);
+
+        assertNotNull(user.getEmail());
+        verify(repository).getUserByEmail(userToSave.getEmail());
+        verify(repository).save(userToSave);
+    }
+
+    @Test
+    @DisplayName("Should reject existing user")
+    void should_reject_existing_user() {
+
+        User userToSave = UserBuilderTest.builder().id(null).build();
+
+        when(repository.getUserByEmail(userToSave.getEmail()))
+                .thenReturn(Optional.of(UserBuilderTest.builder().build()));
+
+        ValidationException ex = assertThrows(ValidationException.class, () ->
+                userService.save(userToSave));
+
+        assertTrue(ex.getMessage().endsWith("registered"));
+
+        verify(repository, never()).save(userToSave);
     }
 
     @Test
     @DisplayName("should return a non-existent empty user")
     void should_return_a_non_existent_empty_user() {
 
-        UserRepository repository = Mockito.mock(UserRepository.class);
-        userService = new UserService(repository);
-
         // como esse já é o comportamento patrão, não precisa
 //        Mockito.when(repository.getUserByEmail("email@hotmail.com"))
 //                .thenReturn(Optional.empty());
 
-        Optional<User> user = userService.getUserByEmail("email@hotmail.com");
+        Optional<User> user = userService.getUserByEmail(EMAIL);
         assertTrue(user.isEmpty());
 
     }
@@ -48,11 +93,6 @@ public class UserServiceMokitoTest {
     @Test
     @DisplayName("Should return a user by email")
     void should_return_a_user_by_email() {
-
-        final String EMAIL = "email@hotmail.com";
-
-        UserRepository repository = mock(UserRepository.class);
-        userService = new UserService(repository);
 
         when(repository.getUserByEmail(EMAIL))
                 .thenReturn(Optional.of(UserBuilderTest.builder().email(EMAIL).build()));
@@ -64,18 +104,17 @@ public class UserServiceMokitoTest {
         verify(repository).getUserByEmail(EMAIL);
         verify(repository, times(1)).getUserByEmail(EMAIL);
         verify(repository, atLeastOnce()).getUserByEmail(EMAIL);
-        verify(repository, never()).getUserByEmail("other@gmail.com");
+        verify(repository, never()).getUserByEmail(EMAIL_OTHER);
         verifyNoMoreInteractions(repository);
+
     }
 
     @Test
     @DisplayName("Should return a user by email by calling the method more than once")
     void should_return_a_user_by_email_by_calling_the_method_more_than_once() {
 
-        final String EMAIL = "email@hotmail.com";
 
-        UserRepository repository = mock(UserRepository.class);
-        userService = new UserService(repository);
+
 
 //        when(repository.getUserByEmail(EMAIL))
 //                .thenReturn(Optional.of(UserBuilderTest.builder().email("email01@gmail.com").build()))
@@ -89,7 +128,6 @@ public class UserServiceMokitoTest {
                         Optional.of(UserBuilderTest.builder().email("email03@gmail.com").build()),
                         Optional.of(UserBuilderTest.builder().email("email04@gmail.com").build()));
 
-
         Optional<User> user = userService.getUserByEmail(EMAIL);
         System.out.println(user);
 
@@ -99,7 +137,7 @@ public class UserServiceMokitoTest {
         user = userService.getUserByEmail(EMAIL);
         System.out.println(user);
 
-        user = userService.getUserByEmail("other@gmail.com");
+        user = userService.getUserByEmail(EMAIL_OTHER);
         System.out.println(user);
 
         user = userService.getUserByEmail(EMAIL);
@@ -107,7 +145,6 @@ public class UserServiceMokitoTest {
 
         user = userService.getUserByEmail(EMAIL);
         System.out.println(user);
-
 
 
         verify(repository, atLeastOnce()).getUserByEmail(EMAIL);
